f7280af7564683a87bed7f356862b1a4279779ff
Your Name <you@example.com>
2022-04-01 15:14:16 +0530

    Retriving Employee data from database

---
+++ main/java/com/bl/FileUtils.java
@@ -0,0 +1,27 @@
+package com.bl;
+
+import java.io.File;
+
+public class FileUtils
+{
+		/**
+		 * created boolean method deleteFiles for deleting files
+		 * 
+		 * @param contentsToDelete
+		 * @return
+		 */
+		public static boolean deleteFiles(File contentsToDelete) 
+		{
+			File[] allContents = contentsToDelete.listFiles();
+			if (allContents != null)
+			{
+				for (File file : allContents)
+				{
+					deleteFiles(file);
+				}
+			}
+			return contentsToDelete.delete();
+		}
+
+}
+
